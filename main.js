99349a222dd1fe621adb16cec5cda3f7ca12fba7
HerryZheng <303983522@qq.com>
2022-04-17 11:11:19 +0800

    完成

--- main.js
+++ main.js
@@ -5,28 +5,61 @@ let $btn = $('.button')
 let $$popups = $$('.popups')
 const App = {
     init() {
-        this.$code = $('#code')
+        this.$codePre = $('#code pre')
         this.$button = $('.button')
         this.$modal = $('#modal')
+        this.$popupGranted = $('.popup-granted')
+        this.$popupDenied = $('.popup-denied')
+
         this.text = ''
+        this.cursor = 0
+        this.canEdit = false
+
         this.bind()
         this.fetchData()
     },
-    bind(){
-
+    bind() {
+        this.$button.onclick = () => {
+            this.hide(this.$modal)
+            this.canEdit = true
+            this.$codePre.classList.add('editable')
+        }
+        document.onkeypress = (e) => {
+            console.log(e)
+            if (e.key === '0') {
+                this.show(this.$popupGranted)
+                this.canEdit = false
+            } else if (e.key === '1') {
+                this.show(this.$popupDenied)
+                this.canEdit = false
+            } else if (e.key === '2') {
+                this.hide(this.$popupGranted)
+                this.hide(this.$popupDenied)
+                this.hide(this.$modal)
+                this.canEdit = true
+            } else if (e.key === 'h') {
+                this.show(this.$modal)
+                this.canEdit = false
+            } else if (this.canEdit) {
+                let str = this.text.substr(0, this.cursor)
+                this.$codePre.innerText = str
+                this.cursor += 10
+            }
+        }
     },
-    show($node){
+    show($node) {
         $node.classList.add('show')
     },
-    hide($node){
+    hide($node) {
         $node.classList.remove('show')
     },
-    fetchData(){
+    fetchData() {
         fetch('https://raw.githubusercontent.com/forkingdog/FDStackView/master/FDStackView.podspec')
-            .then(res.text())
-            .then(res=>{
+            .then(res => res.text())
+            .then(res => {
                 this.text = res
                 this.show(this.$modal)
+
             })
     }
 }
